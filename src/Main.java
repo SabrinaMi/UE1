40501d4f2f54606d6fc45aa1fd038f91ec7fbb16
Mitternöckler Sabrina Andrea <1810653748@stud.fh-kufstein.ac.at>
2018-10-11 11:26:43 +0200

    Alter

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        String nachname = "Mitternoeckler";
-        String vorname = "Sabrina";
-        System.out.println("Hallo mein Name ist "+vorname +" "+nachname +"!");
+        int geburtsjahr = 1993;
+        int alter = 25;
+        System.out.println("Hallo ich bin im Jahr "+geburtsjahr +" geboren und bin nun "+alter +" alt!");
     }
 }
 
