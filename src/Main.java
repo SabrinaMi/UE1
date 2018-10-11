1466ca79371ccfd168fe107bf109f2af1ae47493
Mitternöckler Sabrina Andrea <1810653748@stud.fh-kufstein.ac.at>
2018-10-11 11:31:36 +0200

    Wohnort

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        int geburtsjahr = 1993;
-        int alter = 25;
-        System.out.println("Hallo ich bin im Jahr "+geburtsjahr +" geboren und bin nun "+alter +" alt!");
+        String wohnort = "Kufstein";
+        System.out.println("Hallo ich wohne in "+wohnort+"!");
     }
 }
 
